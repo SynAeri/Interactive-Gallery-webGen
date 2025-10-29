54cf0dba70f7d542a51bf5cdae280a8e84beb412
SynAeri <jordan.maquiran@outlook.com>
2025-10-30 02:27:28 +1100

    updated the gallery components

---
+++ front/app/slideshow/page.js
@@ -0,0 +1,10 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+<Splide aria-label="My Favorite Images">
+  <SplideSlide>
+    <img src="image1.jpg" alt="Image 1"/>
+  </SplideSlide>
+  <SplideSlide>
+    <img src="image2.jpg" alt="Image 2"/>
+  </SplideSlide>
+</Splide>
